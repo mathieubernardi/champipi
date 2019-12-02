c8547117c18b860855800436da314c2b6c3e57a0
mat <mat83bernardi@gmail.com>
2019-12-02 13:17:53 +0100

    server change

--- app/server.py
+++ app/server.py
@@ -11,9 +11,9 @@ from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 #export_file_url = 'https://drive.google.com/drive/folders/1vQRTDO_NlncxSh2hzqRUjzH_Q-huXrBT'
-#export_file_url = 'https://drive.google.com/uc?export=download&id=1sUkhWHHm715I3rbCOPAW6q-XbnL-RKIE'
-#export_file_url = 'https://drive.google.com/file/d/1peznbLXG8zptnSqZtL56smgLFPksEvSp/view?usp=sharing'
-export_file_url = 'https://drive.google.com/file/d/1tSh_AfHa89_eqIYPpH6WO6jghtFIWOKC/view?usp=sharing'
+#export_file_url = 'https://drive.google.com/file/d/1QUWukVw6x7g08RTx468gwFVTIS18yFi1/view?usp=sharing'#lien du .pkl
+#export_file_name = 'good_model.pkl'
+export_file_url='https://drive.google.com/file/d/1tSh_AfHa89_eqIYPpH6WO6jghtFIWOKC/view?usp=sharing'#lien du .pth
 export_file_name = 'good_champi_stage-2.pth'
 
 
@@ -39,15 +39,15 @@ async def download_file(url, dest):
 async def setup_learner():
 	await download_file(export_file_url, path/export_file_name)
 	try:
-        learn = load_learner(path, export_file_name)
+		learn = load_learner(path, export_file_name)
 		return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
+	except RuntimeError as e:
+		if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+			print(e)
+			message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+			raise RuntimeError(message)
+		else:
+			raise
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
