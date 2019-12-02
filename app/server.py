1f9ea259af827c0b658eb0f37577944d454fabdf
mat <mat83bernardi@gmail.com>
2019-12-02 12:14:51 +0100

    new server

--- app/server.py
+++ app/server.py
@@ -9,9 +9,15 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://drive.google.com/drive/folders/1vQRTDO_NlncxSh2hzqRUjzH_Q-huXrBT'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1sUkhWHHm715I3rbCOPAW6q-XbnL-RKIE'
 export_file_url = 'https://drive.google.com/file/d/1peznbLXG8zptnSqZtL56smgLFPksEvSp/view?usp=sharing'
 export_file_name = 'export.pkl'
+#export_file_name = 'export_bearmodel.pkl'
 
+#classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'teddys', 'grizzly', 'blanc', 'pandas']
 classes = ['cepes', 'girolles', 'trompettes', 'chanterelles', 'sanguins','oronges','pied_de_moutons']
 path = Path(__file__).parent
 
