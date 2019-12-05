099f8749bdda13ff8b3629d5e80bbf4fe1d76a48
mat <mat83bernardi@gmail.com>
2019-12-05 17:58:33 +0100

    new champipi appli

--- app/server.py
+++ app/server.py
@@ -9,17 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-#export_file_url = 'https://drive.google.com/drive/folders/1vQRTDO_NlncxSh2hzqRUjzH_Q-huXrBT'
-export_file_url = 'https://drive.google.com/file/d/1QUWukVw6x7g08RTx468gwFVTIS18yFi1/view?usp=sharing'#lien du .pkl
-export_file_name = 'good_model.pkl'
-#export_file_url='https://drive.google.com/file/d/1tSh_AfHa89_eqIYPpH6WO6jghtFIWOKC/view?usp=sharing'#lien du .pth
-#export_file_name = 'good_champi_stage-2.pth'
+export_file_url = 'https://drive.google.com/file/d/1c_0vHvsQrFjIVXY1xxM8_O-T9DAd_Y63/view?usp=sharing'
+export_file_name = 'export.pkl'
 
-
-
-#classes = ['black', 'teddys', 'grizzly', 'blanc', 'pandas']
-classes = ['cepes', 'girolles', 'trompettes', 'chanterelles', 'sanguins','oronges','pied_de_moutons']
+classes = ['cepes', 'girolles', 'trompettes', 'chanterelles', 'sanguins','oronges','pied_de_moutons','mauvais']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -37,39 +30,30 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-	await download_file(export_file_url, path/export_file_name)
-	try:
-		learn = load_learner(path, export_file_name)
-		return learn
-	except RuntimeError as e:
-		if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-			print(e)
-			message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-			raise RuntimeError(message)
-		else:
-			raise
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
+
 @app.route('/')
 async def homepage(request):
     html_file = path / 'view' / 'index.html'
     return HTMLResponse(html_file.open().read())
-	
-	
-#@pp.route("/classify-url",mehods=["GET"])
-#async def classify_url(request):
-#    bytes= await get_bytes(request.query_params["url"])
-#    img=open_image(BytesIO(bytes))
-#    _,_,losses=learner.predict(img)
-#    return JSONResponse({
-#        "predictions": sorted(
-#        zip(champ_learner.data.classes, map(float,losses)),
-#        key=lambda p: p[1], reverse=True)
-#   })
+
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
