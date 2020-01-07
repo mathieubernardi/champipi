09e58fc61fdfc6ec97138434bcdf6cd49db2270c
mat <mat83bernardi@gmail.com>
2020-01-07 15:56:54 +0100

    new version for aws

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,14 @@ RUN apt-get update && apt-get install -y git python3-dev gcc \
 
 COPY requirements.txt .
 
-RUN pip install --upgrade -r requirements.txt
+RUN pip install --upgrade pip
+
+RUN pip install --no-cache-dir -r requirements.txt --upgrade
 
 COPY app app/
 
 RUN python app/server.py
 
-EXPOSE 5000
+EXPOSE 5042
 
 CMD ["python", "app/server.py", "serve"]
--- app/server.py
+++ app/server.py
@@ -1,43 +1,35 @@
-import aiohttp
-import asyncio
-import uvicorn
-from fastai import *
-from fastai.vision import *
-from io import BytesIO
 from starlette.applications import Starlette
-from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
-from functools import partial
-import pickle
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn, aiohttp, asyncio
+from io import BytesIO
 
-#mettre a la fin du lien dropbox =1 sinon sa marche pas
-export_file_url = 'https://www.dropbox.com/s/uvn1z9tk44d91be/champipi_92.pkl?dl=1'
-export_file_name = 'champipi_92.pkl'
+from fastai import *
+from fastai.vision import *
 
-#classes = ['cepes', 'girolles', 'trompettes', 'chanterelles', 'sanguins','oronges','pied_de_moutons','mauvais_a','mauvais_b','mauvais_c','mauvais_d']
-classes = ['cepes', 'chanter_trompet_girol', 'oronges', 'pied_moutons', 'sanguins','veneneux']
+#export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+#export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/o60t9e2j1l0r2lk/champipi_92.7.pth?dl=1'
+export_file_name = 'champipi_92.7.pth'
+
+classes = ['cepes', 'chanterelles_trompettes_girolles', 'oronges', 'pied_moutons', 'sanguins','veneneux']
+#classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-pickle.load=partial(pickle.load, encoding="latin1")
-pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
-#model=torch.load(model_file,map_location=labda storage, loc: storage, pickle_module=pickle)
-
 async def download_file(url, dest):
     if dest.exists(): return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
-
+            with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(export_file_url, path/export_file_name)
     try:
         learn = load_learner(path, export_file_name)
         return learn
@@ -49,28 +41,23 @@ async def setup_learner():
         else:
             raise
 
-
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
-
 @app.route('/')
-async def homepage(request):
-    html_file = path / 'view' / 'index.html'
-    return HTMLResponse(html_file.open().read())
-
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
+    data = await request.form()
+    img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
-
 if __name__ == '__main__':
-    if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+    if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
